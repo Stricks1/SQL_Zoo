c174af16c263875bfdb2832e1d86beed12619935
Stricks1 <gmalheiross@gmail.com>
2020-05-26 19:59:53 -0300

    started 7th exercise list

--- SQL_Zoo_5.sql
+++ SQL_Zoo_5.sql
@@ -32,3 +32,16 @@ SELECT continent, COUNT(name)
 FROM world
 GROUP BY continent
 
+-- 7. For each continent show the continent and number of countries with populations of at least 10 millio
+
+SELECT x.continent, COUNT(x.name)
+FROM world x
+WHERE x.population >= 10000000
+GROUP BY continent
+
+-- 8. List the continents that have a total population of at least 100 million. 
+
+SELECT x.continent
+FROM world x
+GROUP BY continent
+HAVING SUM(population) >= 100000000
